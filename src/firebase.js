0126d7cd35dbbbbb57df1518ce3e0bd5ea5d7067
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-06-08 17:13:34 +1000

    key protection + title changed

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAzduBY-CHGr3pJfD5CXjyc0NweDJuO6lM",
+  apiKey: process.env.REACT_APP_GOOGLE_API,
   authDomain: "disney-clone-stream.firebaseapp.com",
   projectId: "disney-clone-stream",
   storageBucket: "disney-clone-stream.firebasestorage.app",
